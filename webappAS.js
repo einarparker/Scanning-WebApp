8782e21e1b9ae9bd6e69f402770dea6b3a7348f3
einarparker <76267622+einarparker@users.noreply.github.com>
2021-02-08 20:06:26 +0000

    Add second field, rename functions for better readability

--- webappAS.js
+++ webappAS.js
@@ -1,27 +1,41 @@
 var today = Utilities.formatDate(new Date(), "GMT", "EEEE,  dd MMMM yyyy");
+var dayToday = Utilities.formatDate(new Date(), "GMT", "dd MMM yyyy");
 
-function doGet (e) {
+function doGet(e) {
   var tmp = HtmlService.createTemplateFromFile("webapp");
-    
-    var day = today
-    tmp.day = "Today is "+day;
+
+  var day = today;
+  tmp.day = "Today is " + day;
 
   return tmp.evaluate();
 }
- function userClicked(scan){
-   var id = "1TEeuWpLvJv5FZcFnjbQefFVqsDsHCsGwhYEitP1rQpA";
-   var ss = SpreadsheetApp.openById(id)
-   var ws = ss.getSheetByName("new");
-    var sda = ([null,null,null,null,null,scan,null,null,null,"arrived",today]);
-  
-  //ws.setValues(sda)[0]
-   
-  //range.setValues([scan,new Date,"arrived"]);
+function userClicked(scanDpd) {
+  var id = "1TEeuWpLvJv5FZcFnjbQefFVqsDsHCsGwhYEitP1rQpA";
+  var ss = SpreadsheetApp.openById(id);
+  var ws = ss.getSheetByName("new");
+  var dpd = [
+    null,
+    null,
+    null,
+    null,
+    null,
+    scanDpd,
+    null,
+    null,
+    null,
+    "arrived",
+    today,
+  ];
+  ws.appendRow(dpd);
+}
+function eanScanned(scanEan) {
+  var id = "1UJOHgQmJhobZzjtPnA8GEq4FsKuhksGE_Vg_cPgYsQE";
+  var ss = SpreadsheetApp.openById(id);
+  var ws = ss.getSheetByName("new stuff");
+  var ean = [dayToday, null, null, null, null, null, null, null, null, scanEan];
+  ws.appendRow(ean);
+}
 
-   ws.appendRow(sda)
-  
-};
-
- function include(filename){
-   return HtmlService.createHtmlOutputFromFile(filename).getContent();
- }
+function include(filename) {
+  return HtmlService.createHtmlOutputFromFile(filename).getContent();
+}
